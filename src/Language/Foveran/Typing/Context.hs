e87c73b31e1e0e11712cdb99f544d09c933c522a
Robert Atkey <Robert.Atkey@cis.strath.ac.uk>
2011-11-03 19:36:50 +0000

    Refactor representation of contexts

--- src/Language/Foveran/Typing/Context.hs
+++ src/Language/Foveran/Typing/Context.hs
@@ -17,28 +17,34 @@ import           Data.Functor
 import           Data.Monoid
 import qualified Data.Text as T
 import qualified Data.Map as M
+import qualified Data.Set as S
 import           Data.Rec (AnnotRec)
 import           Language.Foveran.Syntax.Checked (Term)
 import           Language.Foveran.Syntax.Identifier
 import           Language.Foveran.Typing.Conversion
 
 -- FIXME: keep track of the order?
-type Context = M.Map Ident (Value, Maybe Value)
+data Context
+    = Context { ctxtGlobal :: M.Map Ident (Value, Maybe Value) }
+
+namesOf :: Context -> S.Set Ident
+namesOf ctxt = M.keysSet (ctxtGlobal ctxt)
 
 emptyContext :: Context
-emptyContext = M.empty
+emptyContext = Context M.empty
 
 ctxtExtend :: Context -> Ident -> Value -> Maybe Value -> Maybe Context
 ctxtExtend ctxt nm ty defn
-    = case M.lookup nm ctxt of
-        Nothing -> Just $ M.insert nm (ty, defn) ctxt
+    = case M.lookup nm (ctxtGlobal ctxt) of
+        Nothing -> Just $ ctxt { ctxtGlobal = M.insert nm (ty, defn) (ctxtGlobal ctxt) }
         Just _  -> Nothing
 
 ctxtExtendFreshen :: Context -> Ident -> Value -> Maybe Value -> (Ident, Context)
 ctxtExtendFreshen ctxt nm ty defn =
-    (freshNm, M.insert freshNm (ty, defn) ctxt)
+    ( freshNm
+    , ctxt { ctxtGlobal = M.insert freshNm (ty, defn) (ctxtGlobal ctxt) } )
     where
-      (_, freshNm) = freshen (M.keysSet ctxt) nm
+      (_, freshNm) = freshen (namesOf ctxt) nm
 
 evalIn :: Term -> Context -> Value
 evalIn t ctxt = evaluate t [] (lookupDef ctxt)
@@ -47,13 +53,13 @@ evalInWithArg :: Term -> Context -> Value -> Value
 evalInWithArg t ctxt v = evaluate t [v] (lookupDef ctxt)
 
 lookupType :: Ident -> Context -> Maybe Value
-lookupType nm ctxt = fst <$> M.lookup nm ctxt
+lookupType nm ctxt = fst <$> M.lookup nm (ctxtGlobal ctxt)
 
 lookupDef :: Context -> Ident -> (Value, Maybe Value)
-lookupDef ctxt nm = case M.lookup nm ctxt of
+lookupDef ctxt nm = case M.lookup nm (ctxtGlobal ctxt) of
                       Nothing        -> error $ "lookupDef: definition not found: " ++ T.unpack nm
                       Just (ty, def) -> (ty, def)
 
 -- FIXME: this is a little confused, probably a better way exists
 contextNameSupply :: Context -> NameSupply a -> a
-contextNameSupply ctxt f = runNameSupply f (M.keysSet ctxt)
+contextNameSupply ctxt f = runNameSupply f (namesOf ctxt)
